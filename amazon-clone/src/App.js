a04ed46d299320996d562d0d9129467160afb4c9
Premchander Satheesh <psathees@uwaterloo.ca>
2023-01-24 01:45:18 -0500

    I hope this is ok

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,15 +2,35 @@ import './App.css';
 import React from "react";
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      <Header />
-      <Home />
-      {/* Body */}
-    </div>
+    <Router>
+      <div className="App">
+        
+        <Switch>
+
+          <Route path="/checkout">
+            <Header />
+            <h1>I AM A CHECKOUT PAGE</h1>
+          </Route>
+          
+          <Route path="/">
+            <Header />
+            <Home />
+            {/* Body */}
+          </Route>
+
+         
+        
+        </Switch>
+        
+      </div>
+    </Router>
+    
   );
 }
 
